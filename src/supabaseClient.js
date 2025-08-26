0c9797802586c9deaa4cb1e368a0d96387243afe
Filmon24 <filmonataklty@gmail.com>
2025-08-26 10:44:12 -0700

    new env32

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://sykdllxqhzeddnnnvbmm.supabase.co"  
-
-
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5a2RsTHhxaHplZGRubm52Ym1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3NjU4NjMsImV4cCI6MjA0MDM0MTg2M30.00-0000000000000000000000000000000000000000"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
