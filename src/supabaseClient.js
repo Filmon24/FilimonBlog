9732043f43abb6dd5a098ebdd3ca46c6e5f83306
Filmon24 <filmonataklty@gmail.com>
2025-08-26 12:10:27 -0700

    new env324

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,5 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+console.log(process.env.REACT_APP_SUPABASE_URL)
+console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Supabase Key Loaded:", !!process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
